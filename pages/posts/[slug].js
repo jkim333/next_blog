7bdfde19f6ea26c5bce6e196957f67329047c03d
Jihyung Kim <kimjihyung3@gmail.com>
2021-09-08 00:00:46 +1000

    Updated post detail page with getStaticProps and getStaticPaths.

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,27 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData } from "../../lib/posts-util";
 
-export default function PostDetailPage() {
-  return <PostContent />;
+export default function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const slug = params.slug;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 }
